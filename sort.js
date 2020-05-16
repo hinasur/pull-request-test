b6d46b2b5e3a0bcbaf470e9c7d1fc2f72f932a60
HiroshiN <qaws252@gmail.com>
2020-05-16 22:46:58 +0900

    Add function reSortNumber

--- sort.js
+++ sort.js
@@ -6,11 +6,21 @@ var sortNumber = function (number) {
         return a < b ? -1 : 1;
     });
  };
+
+ var reSortNumber = function (number) {
+     number.sort((a, b) => b - a);
+ };
  
  var number = [19, 3, 81, 1, 24, 21];
  sortNumber(number);
-var number2 = [1,3,5,7,9,2,4,6,8,];
-sortNumber(number2);
+ 
+ var number2 = [1,3,5,7,9,2,4,6,8,];
+ sortNumber(number2);
+ 
+ var reNumber = [1, 3, 5, 4 , 2];
+ reSortNumber(reNumber);
 
  console.log(number);
  console.log(number2);
+ console.log(reNumber);
+
